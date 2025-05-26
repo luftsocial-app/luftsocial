bbe67e1fb9c3585c520a5adf30e4ddb9c15d7fb2
luftsocial <info@luftsocial.com>
2025-05-26 14:22:22 +0200

    debugging

--- src/app.module.ts
+++ src/app.module.ts
@@ -58,6 +58,9 @@ import { PostAsset } from './asset-management/entities/post-asset.entity';
 import { PublishRecord } from './cross-platform/entities/publish.entity';
 import { RoleGuard } from './guards/role-guard';
 
+console.log({dbOptions: config.get('db.options')});
+
+
 @Module({
   imports: [
     ConfigModule.forRoot({
